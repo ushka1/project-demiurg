a40a41b6b0dd49031e8f8f3bfa6af6f0eca2f6c8
Marcin Starzak <117392254+Jogurtonelle@users.noreply.github.com>
2023-05-22 15:14:19 +0200

    interpreter UI added

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,42 +1,65 @@
 import os
 from dataclasses import dataclass
 
+from kivy.core.text import LabelBase
+from kivy.core.window import Window
+from kivy.properties import StringProperty, ObjectProperty
+from kivymd.app import MDApp
+from kivymd.uix.card import MDCard
+
+from ui.screens.game_screen import GameScreen
+# from screens.creator_screen import CreatorScreen
+
 from runtime.i_runtime import IRuntime
 
 
 @dataclass
-class UI:
+class UI(MDApp):
     """
     UI is responsible for rendering the game state and handling user input.
     """
 
     runtime: IRuntime
+    screen_manager = ObjectProperty()
+
+    def __init__(self, runtime, **kwargs):
+        super(UI, self).__init__(**kwargs)
+        self.runtime = runtime
+
+        LabelBase.register(name='Monoton', fn_regular='ui/assets/Monoton-Regular.ttf')
+        LabelBase.register(name='Nunito', fn_regular='ui/assets/Nunito-VariableFont_wght.ttf')
+        LabelBase.register(name='Nunito_bold', fn_regular='ui/assets/Nunito-Bold.ttf')
+        LabelBase.register(name='Source_code_pro', fn_regular='ui/assets/SourceCodePro.ttf')
+        LabelBase.register(name='Source_code_pro_bold', fn_regular='ui/assets/SourceCodePro-Bold.ttf')
+
+    def on_start(self):
+        self.screen_manager = self.root.ids.screen_manager
+        from kivy.base import EventLoop
+        EventLoop.window.bind(on_keyboard=self.hook_keyboard)
+        # self.rerender()
+
+    def hook_keyboard(self, window, key, *largs):
+        print(key)
+
+        self.screen_manager.current_screen.handle_keyboard(key)
+
+        return True
+
+    def build(self):
+        self.theme_cls.primary_palette = "BlueGray"
+        self.theme_cls.accent_palette = "Red"
+        self.theme_cls.theme_style = "Dark"
 
     def rerender(self):
-        self.clear_terminal()
-
-        current_location = self.runtime.get_current_location()
-        print("Location: " + current_location.name)
-        print(current_location.text)
-
-        if current_location.is_end_location:
-            return
-
-        available_exits = self.runtime.get_available_exits()
-        if available_exits:
-            print("\nAvailable exits:")
-            for key, value in available_exits.items():
-                print(key + ": " + value.text)
-
-        message = self.runtime.get_message()
-        if message:
-            print("\nMessage: " + message)
-
-        self.listen_for_input()
+        self.screen_manager.current_screen.rerender()
 
     def clear_terminal(self):
         os.system('cls' if os.name == 'nt' else 'clear')
 
     def listen_for_input(self):
-        exit_id = input("\nSelect exit: ")
+        exit_id = self.root.ids.text_field.text
         self.runtime.select_exit(exit_id)
+
+
+class NavigationButton(MDCard):
+    text = StringProperty()
